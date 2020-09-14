117eefd8d61436a308ea0389ee23e6b6b495cd4a
Cheena <sh.bazarbay@gmail.com>
2020-09-14 16:15:38 +0600

    Completed Episode 58; made a typo last commit, should've been Episode 57

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,14 +1,14 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-	@foreach(range(1, 8) as $index)
+	@foreach(auth()->user()->follows as $user)
 	<li class="mb-4">
 		<div class="flex items-center text-sm">
 			<img 
-				src="https://avatars.dicebear.com/api/avataaars/{{ $index }}.svg?options[h]=40" 
+				src="{{ $user->getAvatar(40) }}" 
 				alt="" 
 				class="rounded-full mr-2"
-			> John Doe {{ $index }}
+			>{{ $user->name }}
 		</div>
 	</li>
 	@endforeach
